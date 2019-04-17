e92c476b357275c17832fb85c9cc5f2f425a8ad1
Ben Cross <b.cross@lancaster.ac.uk>
2019-04-17 16:22:14 +0100

    Initial

---
+++ Hello World/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hello_World.Controllers
+{
+    [Route("hello/api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("SayHello")]
+        public async Task<IActionResult> SayHello()
+        {
+            return Ok("Hello");
+        }
+    }
+}
---
+++ Hello World/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Hello_World
+{
+    public class Program : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
